dff97da0b05c3e3c76f1f16b2c8e745f920b787e
zzhxufeng <zzhxufeng@163.com>
2023-01-03 13:06:05 +0800

    Mongo DB demo

---
+++ MongoDBCRUD/Models/DatabaseNameModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MongoDBCRUD.Models
+{
+    public record DatabaseNameModel
+    {
+        public string Name { get; set; }
+        public long SizeOnDisk { get; set; }
+        public bool IsEmpty { get; set; }
+
+        public DatabaseNameModel(string name, long sizeOnDisk, bool isEmpty)
+        {
+            this.Name = name;
+            this.SizeOnDisk = sizeOnDisk;
+            this.IsEmpty = isEmpty;
+        }
+    }
+}
