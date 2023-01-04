caaeacb9e2358fa2cf0088b291210cf64c20b88e
zzhxufeng <zzhxufeng@163.com>
2023-01-04 15:03:29 +0800

    rename MVVMSampleAppWpf with Navigation

---
+++ Navigation/ViewModels/ButtonsPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Navigation.ViewModels
+{
+    partial class ButtonsPageViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private string content = "There is something from back end view model.";
+    }
+}
---
+++ Navigation/Views/Buttons.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Navigation.ViewModels;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace MVVMSampleAppWpf.Views
+{
+    /// <summary>
+    /// Buttons.xaml 的交互逻辑
+    /// </summary>
+    public partial class Buttons : Page
+    {
+        public Buttons()
+        {
+            InitializeComponent();
+
+            this.DataContext = new ButtonsPageViewModel();
+
+            /**
+             * The reprint of this message indicates that every time when you navigate to a page, 
+             * this page will just initialize a new object.
+             * 
+             * We can use DI to ensure that, the Page could be different objects, but the context(view model)
+             * still be the same "singleton".
+             * */
+            Debug.WriteLine($"Page: '{this.GetType().Name}' initialized again!");
+        }
+    }
+}
