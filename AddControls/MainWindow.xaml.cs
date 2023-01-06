a65d403c490e69837f9bd5bec2f9ce92eb804df8
zzhxufeng <zzhxufeng@163.com>
2023-01-06 10:49:11 +0800

    Add controls with MVVM

---
+++ AddControls/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using AddControls.ViewModels;
+using System.Windows;
+
+namespace AddControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ AddControls/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace AddControls.ViewModels
+{
+    partial class MainWindowViewModel: ObservableObject
+    {
+        public ObservableCollection<SearchEntryViewModel> MySearchItems { get; set; } = new();
+
+        [RelayCommand]
+        public void AddControl()
+        {
+            MySearchItems.Add(new SearchEntryViewModel());
+        }
+    }
+
+    partial class SearchEntryViewModel: ObservableObject
+    {
+        //Properties for Binding to Combobox and Textbox goes here
+        [ObservableProperty]
+        public string myPropertyInSearchEntryViewModel = "just write some content here.";
+    }
+}
