70931918c0039bf3b07aad3603cbaf92858c0cb8
zzhxufeng <zzhxufeng@163.com>
2023-01-10 12:16:33 +0800

    MongoDB demo

---
+++ TextBoxSizeChange/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace TextBoxSizeChange
+{
+    partial class MainWindowViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<TextBoxViewModel> databaseNames = new();
+
+        public MainWindowViewModel()
+        {
+            databaseNames.Add(new TextBoxViewModel());
+            databaseNames.Add(new TextBoxViewModel());
+        }
+    }
+
+    partial class TextBoxViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private string text = "something";
+    }
+}
