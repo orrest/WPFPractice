ba127d03a4474e9fb147b4961d212a7e34ae23ee
zzhxufeng <zzhxufeng@163.com>
2023-01-19 13:23:46 +0800

    Json serialize and deserialize demo

---
+++ JsonSerilizationDeserilizationDemo/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Windows;
+
+namespace JsonSerilizationDeserilizationDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Text { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string output;
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Product product = new Product
+            {
+                Name = "Apple",
+                ExpiryDate = new DateTime(2008, 12, 28),
+                Price = 3.99M,
+                Sizes = new string[] { "Small", "Medium", "Large" }
+            };
+
+            output = JsonConvert.SerializeObject(product);
+            //{
+            //  "Name": "Apple",
+            //  "ExpiryDate": "2008-12-28T00:00:00",
+            //  "Price": 3.99,
+            //  "Sizes": [
+            //    "Small",
+            //    "Medium",
+            //    "Large"
+            //  ]
+            //}
+            System.Diagnostics.Debug.WriteLine(output);
+            textBlock.Text = output;
+        }
+
+        private void dButton_Click(object sender, RoutedEventArgs e)
+        {
+            Product deserializedProduct = JsonConvert.DeserializeObject<Product>(output);
+            System.Diagnostics.Debug.WriteLine(deserializedProduct.ToString());
+            textBlock.Text = deserializedProduct.ToString();
+        }
+    }
+
+    public record Product
+    {
+        public string Name { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public decimal Price { get; set; }
+        public string[] Sizes { get; set; }
+    }
+}
