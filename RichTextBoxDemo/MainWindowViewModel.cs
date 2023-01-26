a7de407e9511ce52daabd305cd01c00971368954
xufeng <zzhxufeng@163.com>
2023-01-26 22:24:00 +0800

    Working, but richtextbox performance issue.

---
+++ RichTextBoxDemo/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Documents;
+
+namespace RichTextBoxDemo
+{
+    public partial class MainWindowViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        public string autobiography = string.Empty;
+
+        public MainWindowViewModel()
+        {
+            Autobiography = @"<FlowDocument xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""><Paragraph><Bold>Hello World!</Bold></Paragraph></FlowDocument>"; ;
+        }
+    }
+}
---
+++ RichTextBoxDemo/RichTextBoxHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace RichTextBoxDemo
+{
+    public class RichTextBoxHelper
+    {
+        public static string GetDocumentXaml(DependencyObject obj) 
+        { 
+            return (string)obj.GetValue(DocumentXamlProperty); 
+        }
+
+        public static void SetDocumentXaml(DependencyObject obj, string value)
+        {
+            obj.SetValue(DocumentXamlProperty, value);
+        }
+
+        public static readonly DependencyProperty DocumentXamlProperty = DependencyProperty.RegisterAttached
+        (
+            "DocumentXaml",
+            typeof(string),
+            typeof(RichTextBoxHelper),
+            new FrameworkPropertyMetadata
+            {
+                BindsTwoWayByDefault = true,
+                PropertyChangedCallback = (obj, e) =>
+                {
+                    var richTextBox = (RichTextBox)obj;
+                    var xaml = GetDocumentXaml(richTextBox);
+                    Stream sm = new MemoryStream(Encoding.UTF8.GetBytes(xaml));
+                    richTextBox.Document = (FlowDocument)XamlReader.Load(sm);
+                    sm.Close();
+                }
+            }
+        );
+    }
+}
